6d48102b0d87d5a3ef89cd2ae6fe193af946a86f
Alessia Lescano <alessiaklescano@gmail.com>
2025-11-14 22:02:35 -0300

    Added About Works and Contact Sections

---
+++ app/works/page.tsx
@@ -0,0 +1,14 @@
+// COMPONENTS
+
+// ASSETS
+import styles from "@/ui/section.module.scss"
+
+export default function Home() {
+  const { section, siteLogo } = styles
+  return (
+    <div className={section}>
+      <div>
+      </div>
+    </div>
+  )
+}
