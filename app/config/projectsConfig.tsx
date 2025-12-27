d6cf709acf9c6c1b16abe7216587f0985975d4ef
Alessia Lescano <alessiaklescano@gmail.com>
2025-12-27 17:26:36 -0300

    Compressed Images Remade Sitelogo

--- app/config/projectsConfig.tsx
+++ app/config/projectsConfig.tsx
@@ -1,7 +1,7 @@
 // ASSETS
-import almarentals from "@/assets/img/website-thumbnails/website-almarentals.jpg"
-import psicoZephyr from "@/assets/img/website-thumbnails/website-psico-zephyr.png"
-import katyadesign from "@/assets/img/website-thumbnails/website-katyadesign.png"
+import almarentals from "@/assets/img/website-thumbnails/website-almarentals.webp"
+import psicoZephyr from "@/assets/img/website-thumbnails/website-psico-zephyr.webp"
+import katyadesign from "@/assets/img/website-thumbnails/website-katyadesign.webp"
 
 import { faStar, faMobileScreen, faPenRuler } from "@fortawesome/free-solid-svg-icons"
 
